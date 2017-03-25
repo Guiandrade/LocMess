7f5ff9f259dcdf4ef690d8cc6bc7709c42585b0e
António Freire <tonifreire93@gmail.com>
2017-03-25 15:09:25 +0000

    server Routes added

---
+++ Server/locmess/src/main/java/pt/ulisboa/tecnico/cmu/Routes.java
@@ -0,0 +1,40 @@
+package pt.ulisboa.tecnico.cmu;
+import static spark.Spark.*;
+import org.json.JSONObject;
+
+
+public class Routes
+{
+  public static void Post(String endpoint){
+    post(endpoint, (request, response) -> {
+      System.out.println("POST "+endpoint);
+
+
+      return "json";
+
+    });
+  }
+  public static void Get(String endpoint){
+    get(endpoint, (request, response) -> {
+      System.out.println("GET "+endpoint);
+
+      return "json";
+
+    });
+  }
+  public static void Put(String endpoint){
+    put(endpoint, (request, response) -> {
+      System.out.println("PUT "+endpoint);
+
+      return "json";
+
+    });
+  }
+  public static void Delete(String endpoint){
+    delete(endpoint, (request, response) -> {
+      System.out.println("DELETE "+endpoint);
+      return "json";
+
+    });
+  }
+}
--- Server/locmess/src/main/java/pt/ulisboa/tecnico/cmu/Server.java
+++ Server/locmess/src/main/java/pt/ulisboa/tecnico/cmu/Server.java
@@ -1,8 +1,5 @@
 package pt.ulisboa.tecnico.cmu;
 import static spark.Spark.*;
-import org.json.JSONObject;
-import java.io.StringWriter;
-import io.jsonwebtoken.*;
 
 
 public class Server
@@ -12,12 +9,17 @@ public class Server
     System.out.println( "Hello World!" );
     System.out.println( "Server address: http://localhost:" + args[0]);
     port(Integer.parseInt(args[0]));
-    //Routes routes = new Routes();
-    post("/init", (request, response) -> {
-      //curl -X POST -s -D - -d '{"nounce":"12345","publicKey":"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlqs1p0CgN0AxoY2s/99yaohsDbQViDVXbujBbTPeS0rOYo0hBlHPKEvcdlY8ztuX8KFnQRt4HzHXhsSEBZ86DJTNt/MMoSM9FWM5tCRTG9YOH8LYjC4RU4dlvI8uqMNAWOTCzyx+b84TSZDNLoBWA0GgsefRpMFBMmNR2PmXe7OZQHroJd1toPfJ/rnmArKRhQbUUA36qIVgr1rB31kM4igl0Vuy5urxmqVcQ1fEQb4sYh3ssFgeayD4vBNW48RqWMWyC1+ZxLTMditGUsqzhh6keSoB+AZiDnMKl/lT6J9jWkUL5bjp1fDSGM4gml0l1HkCM+wXm+azNtC4+1OV+QIDAQAB"}' http://localhost:8080/register
+    Routes.Get("/login");
+    Routes.Get("/signup");
+    Routes.Post("/signup");
+    Routes.Get("/keypairs");
+    Routes.Get("/profile");
+    Routes.Put("/profile");
+    Routes.Post("/locations");
+    Routes.Get("/locations");
+    Routes.Post("/messages");
+    Routes.Get("/messages");
+    Routes.Delete("/messages");
 
-      return "json";
-
-    });
   }
 }

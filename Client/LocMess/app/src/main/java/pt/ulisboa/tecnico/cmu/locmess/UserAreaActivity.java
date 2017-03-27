337a1f3bd347646576c8315138b0cb2e9ebb0b7d
wazamaisers <tiagomousinhomartins@gmail.com>
2017-03-27 17:02:42 +0100

    Post Message form and populateUser() method [for tests] created

--- Client/LocMess/app/src/main/java/pt/ulisboa/tecnico/cmu/locmess/UserAreaActivity.java
+++ Client/LocMess/app/src/main/java/pt/ulisboa/tecnico/cmu/locmess/UserAreaActivity.java
@@ -1,26 +1,46 @@
 package pt.ulisboa.tecnico.cmu.locmess;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import java.util.ArrayList;
+
+import static android.R.attr.data;
+
 public class UserAreaActivity extends AppCompatActivity {
+
+    ArrayList<Location> locations = new ArrayList<Location>();
+    int MAIN_ACTIVITY_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
-
+        locations = (ArrayList<Location>) getIntent().getSerializableExtra("locations");
         final ImageButton ibGridMenu = (ImageButton) findViewById(R.id.ibGridMenu);
 
         ibGridMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent mainMenuIntent = new Intent(UserAreaActivity.this, MainMenuActivity.class);
-                UserAreaActivity.this.startActivity(mainMenuIntent);
+                mainMenuIntent.putExtra("locations", locations);
+                startActivityForResult(mainMenuIntent,MAIN_ACTIVITY_REQUEST_CODE);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == MAIN_ACTIVITY_REQUEST_CODE) {
+            if(resultCode == Activity.RESULT_OK){
+                ArrayList<Location> locationsUpdated = (ArrayList<Location>) data.getSerializableExtra("locationsUpdated");
+                locations = locationsUpdated;
+            }
+        }
     }
 }

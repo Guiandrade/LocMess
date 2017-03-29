40d2bbd40e4d23e23a713e4cd4eb76011d35e8d4
wazamaisers <tiagomousinhomartins@gmail.com>
2017-03-29 15:40:07 +0100

    Added Time and Key Pair dialogues on PostMessage

--- Client/LocMess/app/src/main/java/pt/ulisboa/tecnico/cmu/locmess/PostMessageActivity.java
+++ Client/LocMess/app/src/main/java/pt/ulisboa/tecnico/cmu/locmess/PostMessageActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
@@ -21,6 +22,7 @@ public class PostMessageActivity extends AppCompatActivity {
 
     ArrayList<Location> locations = new ArrayList<Location>();
     private String time = "";
+    private String date = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +36,8 @@ public class PostMessageActivity extends AppCompatActivity {
         final Spinner sSelectLocation = (Spinner) findViewById(R.id.sSelectLocation);
         final Spinner sSelectPolicy = (Spinner) findViewById(R.id.sSelectPolicy);
         final Button bSetTime = (Button) findViewById(R.id.bSetTime);
+        final Button bSetDate = (Button) findViewById(R.id.bSetDate);
+        final Button bKeyPairs = (Button) findViewById(R.id.bKeyPairs);
         final Button bPostMessage = (Button) findViewById(R.id.bPostMessage);
 
         List<String> spinnerPolicyArray =  new ArrayList<String>();
@@ -62,6 +66,7 @@ public class PostMessageActivity extends AppCompatActivity {
                 Log.d("TEST", etTitle.getText().toString());
                 Log.d("TEST", sSelectPolicy.getSelectedItem().toString());
                 Log.d("TEST", "" + time);
+                Log.d("TEST", "" + date);
             }
         });
 
@@ -82,6 +87,37 @@ public class PostMessageActivity extends AppCompatActivity {
                         timeDialog.cancel();
                     }
                 });
+            }
+        });
+
+        bSetDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dateDialog = new Dialog(PostMessageActivity.this);
+                dateDialog.setContentView(R.layout.date_dialog_layout);
+                dateDialog.show();
+
+                final Button bdpSetDate = (Button) dateDialog.findViewById(R.id.bdpSetDate);
+                final DatePicker dpSetDate = (DatePicker) dateDialog.findViewById(R.id.dpSetDate);
+
+                bdpSetDate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        date = dpSetDate.getDayOfMonth() + "/" +
+                                dpSetDate.getMonth() + "/" +
+                                dpSetDate.getYear();
+                        dateDialog.cancel();
+                    }
+                });
+            }
+        });
+
+        bKeyPairs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog keyPairsDialog = new Dialog(PostMessageActivity.this);
+                keyPairsDialog.setContentView(R.layout.keypairs_dialog_layout);
+                keyPairsDialog.show();
             }
         });
 

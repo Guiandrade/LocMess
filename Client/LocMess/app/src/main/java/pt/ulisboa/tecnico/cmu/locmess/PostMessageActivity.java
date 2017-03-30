7b8d6687df0817eef9162e642f9738a1803b5570
wazamaisers <tiagomousinhomartins@gmail.com>
2017-03-30 22:31:15 +0100

    PostMessageActivity done

--- Client/LocMess/app/src/main/java/pt/ulisboa/tecnico/cmu/locmess/PostMessageActivity.java
+++ Client/LocMess/app/src/main/java/pt/ulisboa/tecnico/cmu/locmess/PostMessageActivity.java
@@ -32,7 +32,10 @@ import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class PostMessageActivity extends AppCompatActivity {
@@ -40,8 +43,11 @@ public class PostMessageActivity extends AppCompatActivity {
     ArrayList<Location> locations = new ArrayList<Location>();
     ArrayAdapter<String> adapterList;
     ArrayAdapter<String> adapterAutoComplete;
-    HashMap<String,Boolean> checkedStatus = new HashMap<String,Boolean>();
+    Map<String,Boolean> checkedStatus = new LinkedHashMap<String,Boolean>();
+    private ArrayList<String> keyPairsWhitelist = new ArrayList<String>();
+    private ArrayList<String> keyPairsBlacklist = new ArrayList<String>();
     int bColor = Color.TRANSPARENT;
+    ListView lvPairs;
     private String time = "";
     private String date = "";
 
@@ -62,27 +68,16 @@ public class PostMessageActivity extends AppCompatActivity {
         final EditText etTitle = (EditText) findViewById(R.id.etTitle);
         final EditText etMessage = (EditText) findViewById(R.id.etMessage);
         final Spinner sSelectLocation = (Spinner) findViewById(R.id.sSelectLocation);
-        final Spinner sSelectPolicy2 = (Spinner) findViewById(R.id.sSelectPolicy2);
-        final Spinner sSelectPolicy1 = (Spinner) findViewById(R.id.sSelectPolicy1);
         final Button bSetTime = (Button) findViewById(R.id.bSetTime);
         final Button bSetDate = (Button) findViewById(R.id.bSetDate);
-        final Button bKeyPairs2 = (Button) findViewById(R.id.bKeyPairs2);
+        final Button bKeyPairsWhiteList = (Button) findViewById(R.id.bKeyPairsWhiteList);
+        final Button bKeyPairsBlackList = (Button) findViewById(R.id.bKeyPairsBlackList);
         final Button bPostMessage = (Button) findViewById(R.id.bPostMessage);
         final TextView tvTime = (TextView) findViewById(R.id.tvTime);
         final TextView tvDate = (TextView) findViewById(R.id.tvDate);
 
         tvTime.setText(setDefaultTime());
         tvDate.setText(setDefaultDate());
-
-        List<String> spinnerPolicyArray =  new ArrayList<String>();
-        spinnerPolicyArray.add("SELECT POLICY");
-        spinnerPolicyArray.add("Whitelist");
-        spinnerPolicyArray.add("Blacklist");
-
-        ArrayAdapter<String> policyAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerPolicyArray);
-        policyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sSelectPolicy2.setAdapter(policyAdapter);
-        sSelectPolicy1.setAdapter(policyAdapter);
 
         List<String> spinnerLocationsArray =  new ArrayList<String>();
         spinnerLocationsArray.clear();
@@ -99,7 +94,6 @@ public class PostMessageActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d("TEST", etTitle.getText().toString());
-                Log.d("TEST", sSelectPolicy2.getSelectedItem().toString());
                 Log.d("TEST", "" + time);
                 Log.d("TEST", "" + date);
             }
@@ -156,7 +150,7 @@ public class PostMessageActivity extends AppCompatActivity {
             }
         });
 
-        bKeyPairs2.setOnClickListener(new View.OnClickListener() {
+        bKeyPairsBlackList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final Dialog keyPairsDialog = new Dialog(PostMessageActivity.this);
@@ -167,8 +161,7 @@ public class PostMessageActivity extends AppCompatActivity {
                 final EditText etPair = (EditText) keyPairsDialog.findViewById(R.id.etPair);
                 final Button bAddPair = (Button) keyPairsDialog.findViewById(R.id.bAddPair);
                 final Button bDeletePair = (Button) keyPairsDialog.findViewById(R.id.bDeletePair);
-                final ListView lvPairs = (ListView) keyPairsDialog.findViewById(R.id.lvPairs);
-                final ArrayList<String> keyPairsList = new ArrayList<String>();
+                lvPairs = (ListView) keyPairsDialog.findViewById(R.id.lvPairs);
 
                 ArrayList<String> keys = new ArrayList<String>();
                 keys.add("Club");
@@ -178,7 +171,7 @@ public class PostMessageActivity extends AppCompatActivity {
                 adapterAutoComplete = new ArrayAdapter<String>(v.getContext(), android.R.layout.simple_dropdown_item_1line, keys);
                 acKey.setAdapter(adapterAutoComplete);
 
-                adapterList = new ArrayAdapter<String>(v.getContext(),android.R.layout.simple_list_item_1, keyPairsList);
+                adapterList = new ArrayAdapter<String>(v.getContext(),android.R.layout.simple_list_item_1, keyPairsBlacklist);
                 lvPairs.setAdapter(adapterList);
 
                 acKey.setOnTouchListener(new View.OnTouchListener(){
@@ -193,7 +186,117 @@ public class PostMessageActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         String pair = acKey.getText().toString() + " = " + etPair.getText().toString();
-                        keyPairsList.add(pair);
+                        keyPairsBlacklist.add(pair);
+                        checkedStatus.put(pair,false);
+                        adapterList.notifyDataSetChanged();
+                        /*String res = "";
+                        for(String str : keyPairsList){
+                            res = res + "||" + str;
+                            Log.d("ADD PAIR",res);
+                        }
+                        res = "";
+                        Iterator it = checkedStatus.entrySet().iterator();
+                        while(it.hasNext()){
+                            Map.Entry entry = (LinkedHashMap.Entry)it.next();
+                            res = res + "||" + entry.getKey() + "-" + entry.getValue();
+                            Log.d("ADD",res);
+                        }*/
+                    }
+                });
+
+                lvPairs.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String keyPair = (String) parent.getItemAtPosition(position);
+
+                        if (checkedStatus.get(keyPair).equals(true)){
+                            parent.getChildAt(position).setBackgroundColor(bColor);
+                            checkedStatus.put(keyPair,false);
+                        }
+                        else{
+                            parent.getChildAt(position).setBackgroundColor(Color.RED);
+                            checkedStatus.put(keyPair,true);
+                        }
+                        String res = "";
+                        /*for (LinkedHashMap.Entry<String, Boolean> entry : checkedStatus.entrySet()) {
+                            res = res + "||" + entry.getKey() + "-" + entry.getValue();
+                            Log.d("CLICK LIST",res);
+                        }*/
+                    }
+                });
+
+                bDeletePair.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        /*String res = "";
+                        for (LinkedHashMap.Entry<String, Boolean> entry : checkedStatus.entrySet()) {
+                            res = res + "||" + entry.getKey() + "-" + entry.getValue();
+                            Log.d("DELETE BEGIN",res);
+                        }*/
+
+
+                        int i = 0;
+                        int e = 0;
+                        for (LinkedHashMap.Entry<String, Boolean> entry : checkedStatus.entrySet()) {
+                            if(entry.getValue().equals(true)){
+                                keyPairsBlacklist.remove(i-e);
+                                lvPairs.getChildAt(i).setBackgroundColor(bColor);
+                                e++;
+                            }
+                            i++;
+                        }
+                        checkedStatus.values().removeAll(Collections.singleton(true));
+                        adapterList.notifyDataSetChanged();
+
+                        /*res = "";
+                        for (LinkedHashMap.Entry<String, Boolean> entry : checkedStatus.entrySet()) {
+                            res = res + "||" + entry.getKey() + "-" + entry.getValue();
+                            Log.d("DELETE END",res);
+                        }*/
+                    }
+                });
+
+
+            }
+        });
+
+        bKeyPairsWhiteList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog keyPairsDialog = new Dialog(PostMessageActivity.this);
+                keyPairsDialog.setContentView(R.layout.keypairs_dialog_layout);
+                keyPairsDialog.show();
+
+                final AutoCompleteTextView acKey = (AutoCompleteTextView) keyPairsDialog.findViewById(R.id.acKey);
+                final EditText etPair = (EditText) keyPairsDialog.findViewById(R.id.etPair);
+                final Button bAddPair = (Button) keyPairsDialog.findViewById(R.id.bAddPair);
+                final Button bDeletePair = (Button) keyPairsDialog.findViewById(R.id.bDeletePair);
+                lvPairs = (ListView) keyPairsDialog.findViewById(R.id.lvPairs);
+
+                ArrayList<String> keys = new ArrayList<String>();
+                keys.add("Club");
+                keys.add("Favourite Colour");
+                keys.add("Relationship Status");
+
+                adapterAutoComplete = new ArrayAdapter<String>(v.getContext(), android.R.layout.simple_dropdown_item_1line, keys);
+                acKey.setAdapter(adapterAutoComplete);
+
+                adapterList = new ArrayAdapter<String>(v.getContext(),android.R.layout.simple_list_item_1, keyPairsWhitelist);
+                lvPairs.setAdapter(adapterList);
+
+                acKey.setOnTouchListener(new View.OnTouchListener(){
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event){
+                        acKey.showDropDown();
+                        return false;
+                    }
+                });
+
+                bAddPair.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String pair = acKey.getText().toString() + " = " + etPair.getText().toString();
+                        keyPairsWhitelist.add(pair);
                         checkedStatus.put(pair,false);
                         adapterList.notifyDataSetChanged();
                     }
@@ -214,24 +317,24 @@ public class PostMessageActivity extends AppCompatActivity {
                         }
                     }
                 });
-                /*
+
                 bDeletePair.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        int i = checkedStatus.size()-1;
-                        for (HashMap.Entry<String, Boolean> entry : checkedStatus.entrySet()) {
+                        int i = 0;
+                        int e = 0;
+                        for (LinkedHashMap.Entry<String, Boolean> entry : checkedStatus.entrySet()) {
                             if(entry.getValue().equals(true)){
-                                keyPairsList.remove(i);
+                                keyPairsWhitelist.remove(i-e);
                                 lvPairs.getChildAt(i).setBackgroundColor(bColor);
-                                Log.d("CONA","i = " + i);
-                                Log.d("CONA","size = " + (checkedStatus.size()-1));
+                                e++;
                             }
-                            i--;
+                            i++;
                         }
                         checkedStatus.values().removeAll(Collections.singleton(true));
                         adapterList.notifyDataSetChanged();
                     }
-                });*/
+                });
 
 
             }
@@ -251,14 +354,31 @@ public class PostMessageActivity extends AppCompatActivity {
         Date date = new Date();
         int hours = date.getHours();
         int minutes = date.getMinutes();
+        String finalHour = "";
+        String finalMinutes = "";
 
-        return hours + ":" + minutes;
+        if(String.valueOf(hours).length() == 1){
+            finalHour = "0" + date.getHours();
+        }
+        else{
+            finalHour = String.valueOf(date.getHours());
+        }
+
+        if(String.valueOf(minutes).length() == 1){
+            finalMinutes = "0" + date.getMinutes();
+        }
+        else{
+            finalMinutes = String.valueOf(date.getMinutes());
+        }
+
+        return finalHour + ":" + finalMinutes;
     }
 
     public String setDefaultDate(){
         Date date = new Date();
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
+        cal.add(Calendar.MONTH,1);
         int year = cal.get(Calendar.YEAR);
         int month = cal.get(Calendar.MONTH) + 1;
         int day = cal.get(Calendar.DAY_OF_MONTH);
